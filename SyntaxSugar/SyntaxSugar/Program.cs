fc5faf91c24c84eee1bd5098c13a4a0dd2166417
Mobin Skaria <mobin0@berkeley.edu>
2022-06-03 07:22:19 -0700

    Second Commit

--- SyntaxSugar/SyntaxSugar/Program.cs
+++ SyntaxSugar/SyntaxSugar/Program.cs
@@ -3,9 +3,21 @@
 namespace SyntaxSugar
 {
     internal class Program
-    {#CCR
+    {//CCR
         static void Main(string[] args)
         {
+            /*int answer = 4;
+              string response;
+              if (answer < 9) 
+                {
+                    response = answer + " is less than nine";
+                }
+                else
+                {
+                    response = answer + " is greater than or equal to nine";
+                }
+             */
+               
             var answer = 4;
             var  response = (answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
             Console.WriteLine(response);
